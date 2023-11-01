24a8201b6006210313a0727e59e48e8d59e84214
uniS2 <s2oie1010@gmail.com>
2023-11-02 00:55:48 +0900

    study: 1일차_5_1 any와 유니언 타입 비교

--- ch01/ex01-05-01.ts
+++ ch01/ex01-05-01.ts
@@ -1,5 +1,13 @@
 // 유니언 타입
+//^ any: 마치 자바스크립트로 작성하는 것처럼 동작
+//^ 유니언 타입: 타입스크립트의 이점을 살리면서 코딩
 (() => {
+  //any를 사용하는 경우
+  function getAge(age: any) {
+    age.toFixed(); // 에러 발생, age의 타입이 any로 추론되기 때문에 숫자 관련된 API를 작성할 때 코드가 자동 완성되지 않는다.
+    return age;
+  }
+
   function print(msg: string | number | boolean): void {
     if (typeof msg === "string") {
       // 타입 가드  //^ 명확하게 무슨 타입인지 알 수 있어 해당 타입 관련 메서드 등 호출 가능
