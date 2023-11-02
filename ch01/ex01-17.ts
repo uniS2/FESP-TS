dc434917b0c95ab1825481ccb46218c08e4b2c5e
uniS2 <s2oie1010@gmail.com>
2023-11-03 00:07:52 +0900

    review: 2일차 01-17 인터페이스 재선언(선언 병합) - 동일한 이름의 인터페이스 중복 선언시 기존 인터페이스에 없는 속성을 추가하여 확장 - 가독성 때문에 `extends` 권장

---
+++ ch01/ex01-17.ts
@@ -0,0 +1,22 @@
+// 인터페이스 선언 병합
+// ex01-16.ts 복사
+(function () {
+  interface Todo {
+    title: string;
+    content: string;
+  }
+
+  interface Todo {
+    _id: number;
+    done: boolean;
+  }
+
+  var todo1: Todo = {
+    title: "할일1",
+    content: "등록할 때 사용",
+    _id: 1,
+    done: true,
+  };
+
+  console.log(todo1);
+})();
